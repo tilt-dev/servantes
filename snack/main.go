dde07cda4c33f08d132b6c39d8f775bc90c0b294
Han Yu <han@windmill.engineering>
2018-08-29 16:25:43 -0400

    servantes: add Snack service

--- snack/main.go
+++ snack/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "The snack of the day is: mochi with green bean")
+		snacks := [...]string{
+			"Spam Musubi",
+			"Pocky Sticks",
+			"Kasugai Gummy",
+			"Green Tea Mochi",
+			"Shrimp-flavored Chips",
+			"Red Bean Rice Cake",
+			"Pretz Sticks",
+			"Peaches in Agar Jelly",
+		}
+
+		rand.Seed(time.Now().Unix())
+		fmt.Fprintf(w, "Your next snack: %s", snacks[rand.Intn(len(snacks))])
+
 	})
 
 	http.ListenAndServe(":8083", nil)
