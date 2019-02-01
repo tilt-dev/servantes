05803ad1875548a7e2f5b2808f5d9244875459f2
dbentley <dbentley>
2019-01-31 18:57:07 -0800

    make demo noisier

--- vigoda/main.go
+++ vigoda/main.go
@@ -7,9 +7,16 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func main() {
+	go func() {
+		for {
+			time.Sleep(2 * time.Second)
+			log.Printf("Server status: All good")
+		}
+	}()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		time := "10:00am"
 
